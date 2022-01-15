ae0cb751c2218a46d45c2703c85b254d20a5916e
UJUAL A S <63001257+Ujual@users.noreply.github.com>
2022-01-15 20:12:54 +0530

    Update profile.dart

--- lib/screens/dashboard/profile.dart
+++ lib/screens/dashboard/profile.dart
@@ -2,38 +2,77 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:ilearn/styling/text_styles.dart';
 
-class Profile extends StatelessWidget {
+void main() {
+  runApp(const Profile());
+}
+
+class Profile extends StatefulWidget {
   const Profile({Key? key}) : super(key: key);
 
   @override
+  _ProfileState createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: SafeArea(
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SvgPicture.asset(
-                'assets/svgs/logo/pageUnderConstruction.svg',
-                height: 200,
-                width: 300,
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children:  [
+            const SizedBox(height: 65,),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Center(child: Icon(Icons.person_outline_rounded,size: 75,color: Color.fromRGBO(237, 180, 38, 1.0),),),
+            ),
+            const SizedBox(height: 20,),
+            const Center(child: Text('Allen Fencer',style: TextStyle(fontSize: 24,color: Color.fromRGBO(
+                0, 74, 173, 1.0)),),),
+            const SizedBox(height: 20,),
+            const Center(child: Text('Flutter Developer | Tech Enthusiast',
+              style: TextStyle(fontSize: 10),),),
+            const SizedBox(height: 20,),
+            Row(mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children:  [
+              Column(
+                children: const [
+                  Center(child: Text('LEARN CREDITS',style: TextStyle(fontSize: 11),),),
+                  SizedBox(height: 10,),
+                  Center(child: Text('1.234',style: TextStyle(fontSize: 24,color: Color.fromRGBO(237, 180, 38, 1.0),fontWeight: FontWeight.bold),),),
+                    ],
               ),
-              Container(
-                padding: EdgeInsets.all(20),
-                margin: EdgeInsets.symmetric(horizontal: 25, vertical: 50),
-                decoration: BoxDecoration(
-                    color: Colors.grey[300],
-                    borderRadius: BorderRadius.circular(25)),
-                child: Text(
-                  'This page is under development.',
-                  style: headingStyle,
-                  textAlign: TextAlign.center,
-                ),
-              )
-            ]),
+              const Padding(padding: EdgeInsets.fromLTRB(15, 0, 15, 0)),
+              Column(
+                children: const [
+                  Center(child: Text('DAY STREAK',style: TextStyle(fontSize: 11),),),
+                  SizedBox(height: 10,),
+                  Center(child: Text('46/100',style: TextStyle(fontSize: 24,color: Color.fromRGBO(237, 180, 38, 1.0),fontWeight: FontWeight.bold),),),
+                ],
+              ),
+            ],),
+            const SizedBox(height: 20,),
+            const Card(child: ListTile(leading: Icon(Icons.person_outline_rounded),
+              title: Text('My Account'),trailing: Icon(Icons.arrow_forward_ios_outlined,size: 15,),),),
+            const SizedBox(height: 15,),
+            const Card(child: ListTile(leading: Icon(Icons.history),
+              title: Text('Purchase History'),trailing: Icon(Icons.arrow_forward_ios_outlined,size: 15,),),),
+            const SizedBox(height: 15,),
+            const Card(child: ListTile(leading: Icon(Icons.help_outline_outlined),
+              title: Text('Help & Support'),trailing: Icon(Icons.arrow_forward_ios_outlined,size: 15,),),),
+            const SizedBox(height: 15,),
+            const Card(child: ListTile(leading: Icon(Icons.settings),
+              title: Text('Settings'),trailing: Icon(Icons.arrow_forward_ios_outlined,size: 15,),),),
+            const SizedBox(height: 15,),
+            const Card(child: ListTile(leading: Icon(Icons.logout),
+              title: Text('Logout'),trailing: Icon(Icons.arrow_forward_ios_outlined,size: 15,),),),
+            const SizedBox(height: 15,),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
+
